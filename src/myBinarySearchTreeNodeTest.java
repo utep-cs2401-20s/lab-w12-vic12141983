ffd0ba58c596e719e15992bae90da41bde513d0e
victor <vherreravi@miners.uteo.edu>
2020-04-17 15:26:24 -0600

    I have completed the lab. Commit date 4/17/2020

--- src/myBinarySearchTreeNodeTest.java
+++ src/myBinarySearchTreeNodeTest.java
@@ -1,14 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class myBinarySearchTreeNodeTest {
-
+    // Test 1. Testing with only subtree to the right no left subtree. Create instance Tree and create binary
+    // Tree with 1 as the root. Sine one is the root the only tree that can be created after is right subtree.
+    // Expectation: Pass
+    // Result: Passed
+    // Reflection I will now add a left subtree check
+    // Expectation Test will fail
+    // Result: Test will fail Null pointer since there is no left Subtree. Condition will have to be added in order to
+    // Check if the either left pointer or right are null to prevent this from happening.
     @org.junit.jupiter.api.Test
     void insert() {
-        int[] arr = {1};
+        int[] arr = {1,5,4,3,2};
         myBinarySearchTreeNode Tree = new myBinarySearchTreeNode(arr);
-        int Root = 0;
+        Tree.insert(6);
+        myBinarySearchTreeNode expect = new myBinarySearchTreeNode(arr);
+        assertEquals(expect.myValue,Tree.myValue);
+        assertEquals(expect.right.myValue,Tree.right.myValue);
+        //assertEquals(expect.left.myValue,Tree.right.myValue);Test failed // null pointer exception. No left Subtree. 
 
-        Tree.insert(1);
     }
 
     @org.junit.jupiter.api.Test
@@ -21,7 +31,7 @@ class myBinarySearchTreeNodeTest {
         myBinarySearchTreeNode tree1 = new myBinarySearchTreeNode(a);
         int expect = 1;
         assertEquals(expect, tree1.size());
-        /* Test2 Purpose to see if Binary tree with no elements be created. I figured that even with no elements
+        /* Test3 Purpose to see if Binary tree with no elements be created. I figured that even with no elements
         // There would be creation of pointer that points to null I was wrong.
         // To correct this an Non-Empty array must be passed to the method otherwise it will continue to fail.
         // Testing Method with an empty array.
@@ -37,7 +47,7 @@ class myBinarySearchTreeNodeTest {
 
     @org.junit.jupiter.api.Test
     void height() {
-        // Test3.
+        // Test4.
         // Testing method with negative data values. To test if values have impact on method.
         // Does not require to check the data only if the node exist.
         // Expectation: Pass
@@ -52,7 +62,7 @@ class myBinarySearchTreeNodeTest {
 
     @org.junit.jupiter.api.Test
     void depth() {
-        // Test4.
+        // Test5.
         /* Testing Method with doubles
         Expectation: Fail.
         Result: Test Failed.
@@ -64,9 +74,9 @@ class myBinarySearchTreeNodeTest {
         int expect = 3;
         assertEquals(expect,tree.depth(2));
         */
-        //Test5.
+        //Test6.
         // Testing method with search in left side
-        // Purpose of test to verify method is working correctly. 
+        // Purpose of test to verify method is working correctly.
         //Expectation: Pass
         // Result: pass
         int [] a = {5,4,3,2,1,6,7,8};
